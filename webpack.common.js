873942a3d5b38476a2b17cdba8274a26852712af
Adil Zeshan <adilzeshan@users.noreply.github.com>
2019-04-19 22:37:13 +0100

    chore(package): update clean-webpack-plugin to version 2.0.1 (#5)
    * chore(package): update clean-webpack-plugin to version 2.0.1
    
    Closes #3
    
    * Update configuration option for CleanWebpackPlugin

--- webpack.common.js
+++ webpack.common.js
@@ -91,7 +91,7 @@ module.exports = {
   },
 
   plugins: [
-    new CleanWebpackPlugin(["dist"]),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "app",
       filename: "index.html",
